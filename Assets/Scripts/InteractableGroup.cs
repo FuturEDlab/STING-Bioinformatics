afad596c031cb896eb6728822acae5a610f93fbe
Horacio Vasquez <vasqueho@mail.gvsu.edu>
2025-12-12 19:31:35 -0500

    Made a few tweaks and added functionality for custom grab point objects to reduce manual actions. Time to move on to adding a glow effect to pick up items

--- Assets/Scripts/InteractableGroup.cs
+++ Assets/Scripts/InteractableGroup.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-// using System.Collections.Generic;
+using System.Collections.Generic;
 // using System.Linq;
 using BNG;
 
@@ -12,22 +12,34 @@ public class InteractableGroup: MonoBehaviour
         if (Application.isPlaying) return;
 
         Grabbable grabbableObject;
+        Transform grabPoint;
         Rigidbody rigidObject;
         if (!applyNeededChildComponents) return;
         
         foreach (Transform child in transform)
         {
+            grabPoint = child.transform.Find("GrabPoint");
             if (child.GetComponent<Grabbable>() == null)
             {
                 grabbableObject = child.gameObject.AddComponent<Grabbable>();
                 grabbableObject.GrabPhysics = GrabPhysics.PhysicsJoint;
                 grabbableObject.RemoteGrabbable = true;
+
+                if (grabbableObject.GrabPoints == null) 
+                {
+                    grabbableObject.GrabPoints = new List<Transform>();
+                }
+
+                if (grabPoint != null)
+                {
+                    grabbableObject.GrabPoints.Add(grabPoint);
+                }
             }
             
             if (child.GetComponent<Rigidbody>() == null)
             {
                 rigidObject = child.gameObject.AddComponent<Rigidbody>();
-                rigidObject.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                rigidObject.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
             }
             
         }
@@ -45,20 +57,32 @@ public class InteractableGroup: MonoBehaviour
     {
 
         Grabbable grabbableObject;
+        Transform grabPoint;
         Rigidbody rigidObject;
         foreach (Transform child in transform)
         {
+            grabPoint = child.transform.Find("GrabPoint");
             if (child.GetComponent<Grabbable>() == null)
             {
                 grabbableObject = child.gameObject.AddComponent<Grabbable>();
                 grabbableObject.GrabPhysics = GrabPhysics.PhysicsJoint;
                 grabbableObject.RemoteGrabbable = true;
+
+                if (grabbableObject.GrabPoints == null) 
+                {
+                    grabbableObject.GrabPoints = new List<Transform>();
+                }
+                
+                if (grabPoint != null)
+                {
+                    grabbableObject.GrabPoints.Add(grabPoint);
+                }
             }
             
             if (child.GetComponent<Rigidbody>() == null)
             {
                 rigidObject = child.gameObject.AddComponent<Rigidbody>();
-                rigidObject.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                rigidObject.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
             }
         }
     }
