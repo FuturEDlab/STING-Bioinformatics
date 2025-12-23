284f3f50ff77e80f0c0342686bd8e48d840bc20c
Horacio Vasquez <vasqueho@mail.gvsu.edu>
2025-12-23 11:22:41 -0500

    the key glow implementation is now done yay

--- Assets/Scripts/GlowRadius.cs
+++ Assets/Scripts/GlowRadius.cs
@@ -6,16 +6,21 @@ using BNG;
 public class GlowRadius : MonoBehaviour
 {
     // [SerializeField] private GameObject GlowObject;
+    [SerializeField] private float MaxGlowDistance = .90f;
+    
     private BNGPlayerController playerController;
     // private Grabbable grabbableComponent;
     private float distance;
     // private Renderer objectAppearance;
     private Material[] rendererMaterials;
+    private Renderer renderer;
+    private Collider objectCollider;
 
     private List<Material> rendMaterials;
     // private Color defaultColor;
     private InteractableGroup parentComponent;
     private bool glowAdded = false;
+    private Vector3 closestPoint;
 
     void AddGlowMaterial(Material glowMat)
     {
@@ -30,11 +35,13 @@ public class GlowRadius : MonoBehaviour
         Debug.Log($"{this.transform.position}");
         parentComponent = GetComponentInParent<InteractableGroup>();
         Debug.Log($"{parentComponent.GlowMaterial}");
+        objectCollider = GetComponent<Collider>();
         // Debug.Log($"{this.transform.localPosition}");
         // grabbableComponent = this.GetComponent<Grabbable>();
         // objectAppearance = GetComponent<Renderer>();
-        rendererMaterials = GetComponent<Renderer>().materials;
-        rendMaterials = new List<Material>(rendererMaterials);
+        // rendererMaterials = GetComponent<Renderer>().materials;
+        renderer = GetComponent<Renderer>();
+        rendMaterials = new List<Material>(renderer.materials);
         // Debug.Log($"{objectAppearance.materials.}");
         Debug.Log($"{parentComponent.GlowMaterial.GetFloat("_Scale")}");
         // defaultColor = objectAppearance.material.color;
@@ -46,21 +53,6 @@ public class GlowRadius : MonoBehaviour
             // playerBody = InputBridge.Instance.GetComponentInChildren<BNGPlayerController>().transform;
             // Debug.Log($"{playerBody.localPosition}");
         }
-        
-        // foreach (var mat in rendererMaterials)
-        // {
-        //     if (mat.shader.name == parentComponent.GlowMaterial.shader.name)
-        //     {
-        //         glowAdded = true;
-        //         break;
-        //     }
-        // }
-        //
-        // if (!glowAdded)
-        // {
-        //     AddGlowMaterial(parentComponent.GlowMaterial);
-        //     glowAdded = true;
-        // }
     }
 
     // Update is called once per frame
@@ -70,25 +62,32 @@ public class GlowRadius : MonoBehaviour
         
         // Debug.Log($"{playerController.transform.position}");
         // Debug.Log($"{playerController.transform.localPosition}");
-        distance = Vector3.Distance(transform.position, playerController.transform.position); 
+        closestPoint = objectCollider.ClosestPoint(playerController.transform.position);
+        // distance = Vector3.Distance(transform.position, playerController.transform.position); 
+        distance = Vector3.Distance(closestPoint, playerController.transform.position); 
         Debug.Log(distance);
 
-        if (distance <= 0.90f)
+        if (distance <= MaxGlowDistance && !glowAdded)
         {
             rendMaterials.Add(parentComponent.GlowMaterial);
-            GetComponent<Renderer>().materials = rendMaterials.ToArray();
+            // GetComponent<Renderer>().materials = rendMaterials.ToArray();
+            renderer.materials = rendMaterials.ToArray();
+            glowAdded = true;
             // parentComponent.GlowMaterial.SetFloat("_Scale", 1.18f);
             // objectAppearance.material.color = Color.cyan;
         }
-        else
+        else if (distance > MaxGlowDistance && glowAdded)
         {
             rendMaterials.Remove(parentComponent.GlowMaterial);
-            GetComponent<Renderer>().materials = rendMaterials.ToArray();
+            // GetComponent<Renderer>().materials = rendMaterials.ToArray();
+            renderer.materials = rendMaterials.ToArray();
+            glowAdded = false;
             // parentComponent.GlowMaterial.SetFloat("_Scale", 1f);
             // objectAppearance.material.color = defaultColor;
         }
         
-        // rendererMaterials = rendMaterials.ToArray();
+        // renderer.materials = rendMaterials.ToArray();
+        // GetComponent<Renderer>().materials = rendMaterials.ToArray();
         
     }
 }
