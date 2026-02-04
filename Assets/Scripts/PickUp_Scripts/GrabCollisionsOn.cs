b99ac405abadba28358a028b7ae94fa70f03ad8a
Horacio Vasquez <vasqueho@mail.gvsu.edu>
2026-02-04 16:46:52 -0500

    Some objects can keep collisions with held objects. Objects snap back on top of table

--- Assets/Scripts/PickUp_Scripts/GrabCollisionsOn.cs
+++ Assets/Scripts/PickUp_Scripts/GrabCollisionsOn.cs
@@ -5,31 +5,35 @@ using System.Collections.Generic;
 public class GrabCollisionsOn : MonoBehaviour
 {
     // [SerializeField] private List<Transform> collidableObjects;
+    // [SerializeField] private List<GameObject> collidableObjects;
     [SerializeField] private List<GameObject> collidableObjects;
+
+    public static HashSet<GameObject> uniqueCollidableObjs;
     // [SerializeField] private GameObject[] collidableObjects;
     // private int colliderCount = 0;
-    // private bool allLayersAdded;
+    private bool allLayersAdded;
+    // public List<GameObject> CollidableObjects => collidableObjects;
     
-#if UNITY_EDITOR
-    private void OnValidate()
-    {
-        if (Application.isPlaying) return;
-        
-        // Iterate through all child transforms of this GameObject
-        foreach (GameObject obj in collidableObjects)
-        {
-            obj.gameObject.layer = LayerMask.NameToLayer("Collidable");
-        }
-    }
-#endif
+// #if UNITY_EDITOR
+//     private void OnValidate()
+//     {
+//         if (Application.isPlaying) return;
+//         
+//         // Iterate through all child transforms of this GameObject
+//         foreach (GameObject obj in collidableObjects)
+//         {
+//             obj.gameObject.layer = LayerMask.NameToLayer("Collidable");
+//         }
+//     }
+// #endif
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // void Start()
     // {
     //     int collidableLayer = LayerMask.NameToLayer("Collidable");
-    //     Debug.Log($"Collidable layer index: {collidableLayer}"); // Should NOT be -1
+    //     // Debug.Log($"Collidable layer index: {collidableLayer}"); // Should NOT be -1
     //    
-    //     foreach (GameObject obj in collidableObjects)
+    //     foreach (GameObject obj in uniqueCollidableObjs)
     //     {
     //         if (obj != null)
     //         {
@@ -39,10 +43,28 @@ public class GrabCollisionsOn : MonoBehaviour
     //         }
     //     }
     // }
-    
+
+    private void Awake()
+    {
+        int collidableLayer = LayerMask.NameToLayer("Collidable");
+        // Debug.Log($"Collidable layer index: {collidableLayer}"); // Should NOT be -1
+       
+        foreach (GameObject obj in collidableObjects)
+        {
+            if (obj != null)
+            {
+                // Debug.Log($"Changing {obj.name} from layer {obj.layer} to {collidableLayer}");
+                obj.gameObject.layer = collidableLayer;
+                // Debug.Log($"After change: {obj.name} is now on layer {obj.layer}");
+            }
+        }
+    }
+
     // Update is called once per frame
     // void Update()
     // {
+    //     // if (allLayersAdded) return;
+    //     
     //     int collidableLayer = LayerMask.NameToLayer("Collidable");
     //     Debug.Log($"Collidable layer index: {collidableLayer}"); // Should NOT be -1
     //    
@@ -52,11 +74,13 @@ public class GrabCollisionsOn : MonoBehaviour
     //         {
     //             // Debug.Log($"Changing {obj.name} from layer {obj.layer} to {collidableLayer}");
     //             obj.layer = LayerMask.NameToLayer("Collidable");
-    //             colliderCount += 1;
+    //             // colliderCount += 1;
     //             // Debug.Log($"After change: {obj.name} is now on layer {obj.layer}");
     //         }
     //     }
-    //     
-    //     if (colliderCount = )
+    //
+    //     allLayersAdded = true;
+    //
+    //     // if (colliderCount = )
     // }
 }
--- Assets/Scripts/PickUp_Scripts/GrabStability.cs
+++ Assets/Scripts/PickUp_Scripts/GrabStability.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using BNG;
 
@@ -12,7 +13,53 @@ public class GrabStability : MonoBehaviour
     private int originalLayer;
     private GameObject groundObj;
     private float defaultYposition;
+    private bool wasAboveTable;
 
+    private RaycastHit hit;
+    // private Gr
+
+    // private void EnableCollidableObjects()
+    // {
+    //     HashSet<Collider> collidableObjSet = GrabCollisionsOn.uniqueCollidableObjs;
+    //     Debug.Log($"self collider: {coll.name}");
+    //
+    //     foreach (Collider obj in collidableObjSet)
+    //     {
+    //         Debug.Log($"held collidable collider: {obj}");
+    //         Physics.IgnoreCollision(coll, obj, false);
+    //     }
+    // }
+    
+    private void CheckTableIntersection()
+    {
+        if (!wasAboveTable) return;
+        
+        RaycastHit table = hit;
+        Collider tableCollider = table.collider;
+        
+        // Check if object is intersecting with this table
+        if (coll.bounds.Intersects(tableCollider.bounds))
+        {
+            // Get table surface height (top of the table)
+            float tableSurfaceY = tableCollider.bounds.max.y;
+
+            // Check if object center is mostly above or below table surface
+            float objectCenterY = coll.bounds.center.y;
+
+            if (objectCenterY > tableSurfaceY)
+            {
+                // Mostly above - snap to table surface
+                Vector3 pos = transform.position;
+                pos.y = tableSurfaceY + coll.bounds.extents.y;
+                transform.position = pos;
+
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+            }
+            // If mostly below, let it fall (do nothing)
+        }
+    }
+    
     private void CheckObjBelowFloor()
     {
         // float fallThreshold = groundObj.transform.position.y - 5;
@@ -22,6 +69,7 @@ public class GrabStability : MonoBehaviour
         // Check if object is intersecting with player
         if (coll.bounds.Intersects(parentObject.PlayerCollider.bounds))
         {
+            // Physics.IgnoreCollision(coll, parentObject.PlayerCollider, true);
             // Calculate spawn distance based on object and player size
             float objectRadius = Mathf.Max(coll.bounds.extents.x, coll.bounds.extents.z);
             float playerRadius = Mathf.Max(parentObject.PlayerCollider.bounds.extents.x,
@@ -36,6 +84,7 @@ public class GrabStability : MonoBehaviour
         
             rb.linearVelocity = Vector3.zero;
             rb.angularVelocity = Vector3.zero;
+            // Physics.IgnoreCollision(coll, parentObject.PlayerCollider, false);
         }
         // Check if object's bottom is below floor (and not in player)
         else if (coll.bounds.min.y < floorHeight)
@@ -55,6 +104,7 @@ public class GrabStability : MonoBehaviour
         //     transform.position = pos;
         //     // transform.position.y = defaultYposition;
         // }
+        // Physics.IgnoreCollision(coll, parentObject.PlayerCollider, false);
     }
     
     void Start()
@@ -66,6 +116,7 @@ public class GrabStability : MonoBehaviour
         originalLayer = gameObject.layer;
         groundObj = parentObject.Ground;
         defaultYposition = transform.position.y;
+        // private RayCastHit hit;
     }
 
     void Update()
@@ -74,30 +125,81 @@ public class GrabStability : MonoBehaviour
         // {
         //     Debug.Log($"{transform.name}: {transform.position.y}, Ground: {ground.transform.position.y}");
         // }
+        // Debug.Log($"{transform.name}: {transform.position.y}");
+        // if (transform.CompareTag("Table"))
+        // {
+        //     Debug.Log("is a Table");
+        //     return;
+        // }
+
+        if (Physics.Raycast(coll.bounds.center, Vector3.down, out hit, 10))
+        {
+            // Check if hit object has "Table" tag
+            if (hit.transform.CompareTag("Table") || (hit.collider.transform.parent != null && hit.collider.transform.parent.CompareTag("Table")))
+            {
+                // Debug.Log($"{transform.name} hit [{hit.transform.name}]");
+                // Debug.Log($"{hit.transform.name}: {hit.transform.position.y}");
+                wasAboveTable = true;
+                // tableCollider = hit.collider;
+                // tableSurfaceY = hit.point.y;
+                // return true;
+            }
+        }
 
         if (!grabbable || !rb) return;
         if (!groundObj) return;
         
-        Debug.Log($"{transform.name}: {transform.position.y}, Ground: {groundObj.transform.position.y}");
+        // Debug.Log($"{transform.name}: {transform.position.y}, Ground: {groundObj.transform.position.y}");
 
         if (grabbable.BeingHeld)
         {
             Physics.IgnoreCollision(coll, parentObject.PlayerCollider, true);
             gameObject.layer = LayerMask.NameToLayer("Grabb");
+            // EnableCollidableObjects();
         }
         else
         {
             Physics.IgnoreCollision(coll, parentObject.PlayerCollider, false);
+            // Don't reset layer if this is a collidable object
+            // if (!GrabCollisionsOn.uniqueCollidableObjs.Contains(gameObject))
+            // { 
+            //     gameObject.layer = originalLayer;
+            // }
+            // else if (GrabCollisionsOn.uniqueCollidableObjs.Contains(gameObject))
+            // {
+            //     gameObject.layer = LayerMask.NameToLayer("Collidable");
+            // }
             gameObject.layer = originalLayer;
         }
         
         // This is right after releasing object to prevent excessive spinning
         if (wasHeldLastFrame && !grabbable.BeingHeld)
         {
+            CheckTableIntersection();
             CheckObjBelowFloor();
             rb.angularVelocity = Vector3.ClampMagnitude(rb.angularVelocity, 10f);
+            Physics.IgnoreCollision(coll, parentObject.PlayerCollider, true); // At 4-4:30 PM Feb 4,
+            // test player jump glitch when the object barely touches player's stomach area!! 
         }
         
         wasHeldLastFrame = grabbable.BeingHeld;
     }
+    
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     // Tags LeftHand and RightHand are associated with the Grabber child objects
+    //     // from LeftController object and RightController object (XR Rig Full Body,
+    //     // the parent object, contains these 2 Controller objects)
+    //     
+    //     if (other.CompareTag("Table"))
+    //     {
+    //         Debug.Log($"Table touched {other.name}");
+    //         // isLeftHandNear = true;
+    //     }
+    //     else if (other.CompareTag("Table"))
+    //     {
+    //         Debug.Log($"Table did not touch {other.name}");
+    //         // isRightHandNear = true;
+    //     }
+    // }
 }
