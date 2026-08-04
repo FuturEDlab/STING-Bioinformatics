2a2111fd1186c6376f48f9c653efa9e32bce04fc
Erion Adams <adamerio@gvsu.edu>
2026-08-04 16:53:27 -0400

    Teleportation, grabbing, and UI interaction with ray

--- Assets/Scripts/Player Hands/TeleportationActivator.cs
+++ Assets/Scripts/Player Hands/TeleportationActivator.cs
@@ -5,12 +5,14 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 public class TeleportationActivator : MonoBehaviour
 {
     public XRRayInteractor teleportInteractor;
+    public XRRayInteractor rayInteractor;
     public InputActionProperty teleportActivatorAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         teleportInteractor.gameObject.SetActive(false);
         teleportActivatorAction.action.performed += Action_performed;
+        rayInteractor.uiHoverEntered.AddListener(x => DisableTeleportRay());
     }
 
     // Update is called once per frame
@@ -24,6 +26,19 @@ public class TeleportationActivator : MonoBehaviour
 
     private void Action_performed(InputAction.CallbackContext obj)
     {
+        if (rayInteractor && rayInteractor.IsOverUIGameObject())
+        {
+            return;
+        }
+        {
+            rayInteractor.enabled = false;
+        }
         teleportInteractor.gameObject.SetActive(true);
+    }
+
+    public void DisableTeleportRay()
+    {
+        teleportInteractor.gameObject.SetActive(false);
+        rayInteractor.enabled = true;
     }
 }
